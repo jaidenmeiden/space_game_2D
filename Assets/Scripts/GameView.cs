f18339ea5364a8c83485035d521c7ee3e1035fe7
Jaiden Meiden <jaidenmeiden@gmail.com>
2021-12-17 08:13:17 +0100

    feat: Add menu view script

---
+++ Assets/Scripts/GameView.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameView : MonoBehaviour
+{
+    public Text coinsText, scoresText, maxScoreText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.SharedInstance.currentGameState == GameState.InGame)
+        {
+            int coins = 0;
+            float score = 0;
+            float maxScore = 0;
+            
+            coinsText.text = coins.ToString();
+            scoresText.text = "Score: " + score.ToString("f1");
+            maxScoreText.text = "Max: " + maxScore.ToString("f1");
+        }
+    }
+}
